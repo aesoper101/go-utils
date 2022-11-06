74c368879d9ba5b78b5085636037d9aa49e2bcae
aesoper <weilanzhuan@163.com>
2022-11-07 00:47:40 +0800

    feat: add i18n validator

--- validatorx/translation.go
+++ validatorx/translation.go
@@ -5,6 +5,8 @@ import (
 	"github.com/aesoper101/go-utils/validatorx/locales/zh"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
+	zhTranslations "github.com/go-playground/validator/v10/translations/zh"
 	"reflect"
 	"strings"
 )
@@ -13,9 +15,11 @@ func RegisterDefaultTranslations(v *validator.Validate, trans ut.Translator) {
 	switch trans.Locale() {
 	case "zh":
 		_ = zh.RegisterDefaultTranslations(v, trans)
+		_ = zhTranslations.RegisterDefaultTranslations(v, trans)
 		return
 	case "en":
 		_ = en.RegisterDefaultTranslations(v, trans)
+		_ = enTranslations.RegisterDefaultTranslations(v, trans)
 		return
 	}
 }
